fcfb01ec188b927d4a9e5291c3353b6fad45ff33
apex-creator <ayushtalkar28@gmail.com>
2026-02-11 00:37:03 +0530

    removed the clutter for retry

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,75 +9,7 @@ public class main {
 
     static void main(String[] args) throws Exception {
 
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode node = mapper.createObjectNode();
-        JsonNode jsonNode = mapper.readTree(new File("register.json"));
 
-
-        System.out.println("intiating SAMA...");
-
-        String ipv4 = null;
-
-        while (ipv4 == null) {
-            ipv4 = networkUtils.FindIp();
-            if (ipv4 == null) {
-                System.out.println("Couldn't connect to the network...");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    System.out.println("interrupted, exiting...");
-                    return;
-                }
-            }
-        }
-        if (jsonNode.has(ipv4)) {
-            String status = jsonNode.get(ipv4).asText();
-
-        } else {
-
-            System.out.println("What's your designation in the network> ?");
-            System.out.println("dasa  > 1.");
-            System.out.println("swami > 2.");
-            node.put(ipv4, "dasa");
-        }
-        Scanner designation = new Scanner(System.in);
-        System.out.print("Please enter your designation >>> ");
-        int input = Integer.parseInt(designation.nextLine());
-
-        switch (input) {
-            case 1:
-                String PathToSync = null ;
-                if (jsonNode.has("FIle path: ")){
-                    PathToSync = jsonNode.get("File path ").asText();
-                    System.out.println("Memeory Exists");
-                }
-
-                if (PathToSync == null || PathToSync.isEmpty()){
-                    System.out.print("Enter the directory you want to synchronise, ");
-                    PathToSync = designation.nextLine();
-                    node.put("File path: ", PathToSync);
-                }
-                mapper.writeValue(new File("register.json"), node);
-                Dasa dasa = new Dasa();
-                String finalPathToSync = PathToSync;
-                Thread access = new Thread(() -> {
-                    try {
-                        dasa.slave(finalPathToSync);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-                access.start();
-
-                break;
-            case 2:
-                Swami master = new Swami();
-                master.master();
-                break;
-            default:
-
-        }
     }
 
 }
